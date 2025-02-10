b51347d719ebf2cfbc72fce05ea38079b9a3cd2a
unknown <TheMatrix88888@gmail.com>
2025-02-10 08:07:17 +0800

    Update to password functionality + animations + paper fly out test

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 function checkPassword() {
     const password = document.getElementById("password").value.trim().toLowerCase();
-    const validPasswords = ["поля", "полина", "полечка", "апполинария", "аполинария"];
+    const validPasswords = ["поля", "полина", "полечка", "апполинария"];
+    const passwordContainer = document.getElementById("password-container");
+    const letterContainer = document.querySelector(".letter-container");
+    const paper = document.querySelector(".paper");
+    const content = document.getElementById("content");
 
-    if (validPasswords.includes(password)) {
-        document.getElementById("password-container").classList.add("hidden");
-        const content = document.getElementById("content");
-        content.classList.remove("hidden");
+    if (validPasswords.includes(password) || true) { // true - for testing purposes
+        letterContainer.classList.add("open");
+        setTimeout(() => {
+            paper.style.zIndex = -1;
+            paper.classList.add("flyOut");
+        }, 1500);
+        setTimeout(() => {
+            passwordContainer.classList.add("hidden");
+            letterContainer.classList.add("hidden");
+        }, 3000);
+        setTimeout(() => {
+            passwordContainer.style.display = "none";
+            letterContainer.style.display = "none";
+            content.classList.remove("hidden");
+            content.classList.add("revealed");
+        }, 4000);
     } else {
         alert("Неправильна!😭💔");
     }
-}
-
-
-function ValentineYes() {
-
-}
-
-function ValentineNo() {
-
 }
