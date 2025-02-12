27bcd7919aed007c362638e2a6295a4edf3da4e7
unknown <TheMatrix88888@gmail.com>
2025-02-13 07:20:44 +0800

    Final touches

--- script.js
+++ script.js
@@ -5,12 +5,17 @@ function checkPassword() {
     const letterContainer = document.querySelector(".letter-container");
     const paper = document.querySelector(".paper");
     const content = document.getElementById("content");
+    const bgaudio = document.getElementById("bg-music");
+    bgaudio.volume = 0.05;
+    paper_audio = new Audio('mp3/paper.mp3');
+    paper_audio.volume = 0.8;
 
-    if (validPasswords.includes(password) || true) { // TODO remove true - for testing purposes
+    if (validPasswords.includes(password)) {
         letterContainer.classList.add("open");
         setTimeout(() => {
             paper.style.zIndex = 0;
             paper.classList.add("flyOut");
+            paper_audio.play().catch(() => { });
         }, 1500);
         setTimeout(() => {
             passwordContainer.classList.add("hidden");
@@ -18,6 +23,7 @@ function checkPassword() {
             document.body.classList.add("background-changed");
         }, 3000);
         setTimeout(() => {
+            bgaudio.play().catch(() => { });
             passwordContainer.style.display = "none";
             letterContainer.style.display = "none";
             content.classList.remove("hidden");
@@ -36,7 +42,7 @@ document.addEventListener("DOMContentLoaded", () => {
     plushSound.volume = 0.5;
 
     plush.addEventListener('click', () => {
-        plushSound.play().catch(() => {});
+        plushSound.play().catch(() => { });
         plush.classList.add("squish");
     });
 
@@ -48,6 +54,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function ValentineYes() {
     const yes_content = document.getElementById('yes-content');
+    const party_horn = new Audio('mp3/party_horn.mp3');
+    party_horn.volume = 0.6;
+    party_horn.play().catch(() => { });
     yes_content.classList.remove('hidden');
     yes_content.classList.add('revealed');
+}
+
+function ValentineNo() {
+    const valentine_no = document.getElementById('valentine-no');
+    const WTFSound = new Audio('mp3/WTF.mp3');
+    WTFSound.volume = 0.5;
+    valentine_no.classList.add("move-left");
+    WTFSound.play().catch(() => { });
+    valentine_no.addEventListener('animationend', () => {
+        valentine_no.classList.remove("move-left");
+    });
 }
