723c8e078c98d8ab03ca194d4f9aeacc76470045
unknown <TheMatrix88888@gmail.com>
2025-02-12 15:50:14 +0800

    Plush works + changed paper background

--- script.js
+++ script.js
@@ -6,15 +6,16 @@ function checkPassword() {
     const paper = document.querySelector(".paper");
     const content = document.getElementById("content");
 
-    if (validPasswords.includes(password) || true) { // true - for testing purposes
+    if (validPasswords.includes(password) || true) { // TODO remove true - for testing purposes
         letterContainer.classList.add("open");
         setTimeout(() => {
-            paper.style.zIndex = -1;
+            paper.style.zIndex = 0;
             paper.classList.add("flyOut");
         }, 1500);
         setTimeout(() => {
             passwordContainer.classList.add("hidden");
             letterContainer.classList.add("hidden");
+            document.body.classList.add("background-changed");
         }, 3000);
         setTimeout(() => {
             passwordContainer.style.display = "none";
@@ -25,4 +26,28 @@ function checkPassword() {
     } else {
         alert("Неправильна!😭💔");
     }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const plush = document.querySelector('.plush');
+    if (!plush) return;
+
+    const plushSound = new Audio('mp3/plush.mp3');
+    plushSound.volume = 0.5;
+
+    plush.addEventListener('click', () => {
+        plushSound.play().catch(() => {});
+        plush.classList.add("squish");
+    });
+
+    plush.addEventListener('animationend', () => {
+        plush.classList.remove("squish");
+    });
+});
+
+
+function ValentineYes() {
+    const yes_content = document.getElementById('yes-content');
+    yes_content.classList.remove('hidden');
+    yes_content.classList.add('revealed');
 }
